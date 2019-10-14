43623d719a60f72dd69105473eecad9a1ce274ef
lanbingsiyu <1135293084@qq.com>
2019-10-14 19:43:35 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[ ] args){
+		System.out.print("1");
+		System.out.print("hello world");
+		System.out.print("2");
+		System.out.print("hello world");
+	}
+}
